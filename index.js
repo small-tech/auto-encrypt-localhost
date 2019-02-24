ce45ad494105101f2ac1047fb0023a3394cb18cb
Aral Balkan <mail@ar.al>
2019-02-24 17:36:11 +0000

    Silent operation

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const childProcess = require('child_process')
 const _platform = os.platform()
 const _architecture = os.arch()
 
-console.log(`${_platform} ${_architecture}`)
-
 const platformMap = {
   linux: 'linux',
   darwin: 'darwin',
@@ -30,39 +28,31 @@ const version = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version
 
 let mkcertBinary = path.join(__dirname, 'mkcert-bin', `mkcert-v${version}-${platform}-${architecture}`)
 
-console.log('mkcertBinary', mkcertBinary)
-
 if (platform === 'windows') mkcertBinary += '.exe'
-
-//
-// Check if your local certificate authority and your local keys exist.
-// By convention, these are stored in:
-//
-// CA: ~/.nodecert/mkcert
-// P12 file: ~/.nodecert/localhost.pem
-// Key file: ~/.nodecert/localhost-key.pem
-//
 
 const homeDir = os.homedir()
 const nodecertDir = path.join(homeDir, '.nodecert')
 
-if (fs.existsSync(path.join(nodecertDir, 'rootCA.pem')) && fs.existsSync(path.join(nodecertDir, 'rootCA-key.pem')) && fs.existsSync(path.join(nodecertDir, 'localhost.pem')) && fs.existsSync(path.join(nodecertDir, 'localhost-key.pem'))) {
-  console.log('> Local certificate authority and certificates already exist.')
-} else {
+// Check if the local certificate authority and local keys exist.
+function allOK() {
+  return fs.existsSync(path.join(nodecertDir, 'rootCA.pem')) && fs.existsSync(path.join(nodecertDir, 'rootCA-key.pem')) && fs.existsSync(path.join(nodecertDir, 'localhost.pem')) && fs.existsSync(path.join(nodecertDir, 'localhost-key.pem'))
+}
+
+let log = []
+
+if (!allOK()) {
   // Create certificates.
-  console.log('> Creating local certificate authority and certificates…')
   if (!fs.existsSync(nodecertDir)) {
     fs.mkdirSync(nodecertDir)
   }
   childProcess.execFile(mkcertBinary, ['-install'], {env: { CAROOT: nodecertDir, PATH: process.env.PATH},}, (error, stdout, stderr) => {
-    console.log('error', error)
-    console.log('stdout', stdout)
-    console.log('stderr', stderr)
-
+    log = log.concat([error, stdout, stderr])
     childProcess.execFile(mkcertBinary, [`-key-file=${path.join(nodecertDir, 'localhost-key.pem')}`, `-cert-file=${path.join(nodecertDir, 'localhost.pem')}`, 'localhost', '127.0.0.1', '::1'], {env: { CAROOT: nodecertDir }}, (error, stdout, stderr) => {
-      console.log('error', error)
-      console.log('stdout', stdout)
-      console.log('stderr', stderr)
+      log = log.concat([error, stdout, stderr])
+      if (!allOK()) {
+        console.log(log)
+        process.exit(1)
+      }
     })
   })
 }
