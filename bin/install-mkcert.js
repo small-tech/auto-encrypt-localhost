ab9519b1fbd2a5cbf09356e725de109702e2fc7d
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-03-04 15:37:51 +0000

    Replace mkcert update script with mkcert install script

---
+++ bin/install-mkcert.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Downloads and installs the version of mkcert specified in lib/mkcert.js
+// for the platform this script is running on.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import https from 'https'
+import fs from 'fs-extra'
+import path from 'path'
+
+import { binaryName } from '../lib/mkcert.js'
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+async function secureGet (url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, response => {
+      const statusCode = response.statusCode
+      const location = response.headers.location
+
+      // Reject if it’s not one of the status codes we are testing.
+      if (statusCode !== 200 && statusCode !== 302) {
+        reject({statusCode})
+      }
+
+      let body = ''
+      response.on('data', _ => body += _)
+      response.on('end', () => {
+        resolve({statusCode, location, body})
+      })
+    })
+  })
+}
+
+async function secureStreamToFile (url, filePath) {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createWriteStream(filePath)
+    https.get(url, response => {
+      response.pipe(fileStream)
+      fileStream.on('finish', () => {
+        fileStream.close()
+        resolve()
+      })
+      fileStream.on('error', error => {
+        fs.unlinkSync(filePath)
+        reject(error)
+      })
+    })
+  })
+}
+
+//
+// Install the latest mkcert binary.
+//
+
+console.log('')
+console.log(' Installing latest mkcert binary')
+console.log(' ═══════════════════════════════')
+console.log('')
+
+// This is the directory we will save the binary into.
+// (Placing it here for backwards compatibility with < v7.0.0 releases).
+const mkcertBinariesDirectory = path.resolve(path.join(__dirname, '..', 'mkcert-bin'))
+
+// Delete and recreate the mkcert-bin folder.
+fs.removeSync(mkcertBinariesDirectory)
+fs.mkdirpSync(mkcertBinariesDirectory)
+
+const mkcertBinaryUrl = `https://github.com/FiloSottile/mkcert/releases/download/v${latestMkcertVersion}/${binaryName}`
+
+const binaryRedirectUrl = (await secureGet(mkcertBinaryUrl)).location
+const binaryPath = path.join(mkcertBinariesDirectory, mkcertBinary.binaryName)
+await secureStreamToFile(binaryRedirectUrl, binaryPath)
+
+console.log(' Done.\n')
